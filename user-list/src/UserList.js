94a3bc025f940362686338ae1cf6db23bc6a4fd8
Jacob Tidiakh Ndiapaly Ndiaye <jacobndiaye0113@gmail.com>
2024-10-19 02:51:27 -1200

    add API

---
+++ user-list/src/UserList.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function UserList() {
+  const [listOfUser, setListOfUser] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setListOfUser(response.data);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la récupération des données :", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Liste des Utilisateurs</h1>
+      <ul>
+        {listOfUser.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
